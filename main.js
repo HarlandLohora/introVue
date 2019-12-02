5a71ea63f71c5cb7adff62980ee02c5fff0ddf73
Harland Lohora <lohoraharland@gmail.com>
2019-12-01 21:45:52 -0600

    Working with data

--- main.js
+++ main.js
@@ -2,6 +2,18 @@ let product = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        description: 'This is my description.'
+        image: './img/green.jpg',
+        inStock: 0,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'Green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'Blue'
+            }
+        ]
     } 
 })
